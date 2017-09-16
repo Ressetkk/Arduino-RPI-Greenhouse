3c225d564222e492e04888906c38ef6209744f6d
Kamil Kasperski <ressetkk@gmail.com>
2017-09-16 23:34:45 +0200

    finished master script (kinda)

--- masterOfPuppets.py
+++ masterOfPuppets.py
@@ -1,11 +1,37 @@
 import smbus
 from time import sleep
 from struct import unpack
+import sqlite3 as lite
 
 bus = smbus.SMBus(1)
 address = 0x03
 
-debug = [20,17,19, False]
+con = None
+con = lite.connect('db.sqlite3')
+
+def sendToDB():
+    cur = con.cursor()
+    data = read()
+    cur.execute("update main_app_sensors set value=%s where id=0" %data[0])
+    cur.execute("update main_app_sensors set value=%s where id=1" %data[1])
+    cur.execute("update main_app_sensors set value=%s where id=2" %data[2])
+    cur.execute("update main_app_sensors set value=%s where id=3" %data[3])
+    cur.execute("update main_app_buttons set state=%s where id=1" %1 if data[5] == True else 0)
+    cur.execute("update main_app_buttons set state=%s where id=2" %1 if data[6] == True else 0)
+    con.commit()
+
+def getFromDB():
+    cur = con.cursor()
+    data = []
+    for val in (4,5,6):
+        cur.execute("select value from main_app_sensors where id=%s" % val)
+        data.append(cur.fetchone()[0])
+    cur.execute("select state from main_app_buttons where id=0")
+    data.append(True if cur.fetchone()[0] == 1 else False)
+    send(data)
+
+
+debugSendPacket = [15,20,1, True]
 
 def send(data):
     """
@@ -19,32 +45,20 @@ def read():
     """
     this function receives packed data about all sensors, buttons and local variables
     """
+    # packet description
+    # DS18B20 temp C, DHT temp C, DHT humidity %, soil moisture %, IN1 state (LED), IN2 state (PUMP), IN3 state (HEAT), wanted temperature, wanted soil moisture, temp offset
     I2CPacket = []
     I2CPacket =  bus.read_i2c_block_data(address, 0, 19)
     hexdata = ''.join([chr(item) for item in I2CPacket])
-#    print I2CPacket
     return unpack('fffb???bbb',hexdata)
 
-def parse(packet):
-    """
-    method parses received data and assigns them to proper variables
-    For debug purposes
-    """
-
-    # packet description
-    # DS18B20 temp C, DHT temp C, DHT humidity %, soil moisture %, IN1 state (LED), IN2 state (PUMP), IN3 state (HEAT), wanted temperature, wanted soil moisture, temp offset
-    DS18B20_temp = packet[0]
-    DHT_temp = packet[1]
-    DHT_hum = packet[2]
-    soil_moist = packet[3]
-    in1_state = packet[4]
-    in2_state = packet[5]
-    in3_state = packet[6]
-    wanted_temp = packet[7]
-    wanted_soil = packet[8]
-    temp_offset = packet[9]
-
-    print "External temperature: " + str(DS18B20_temp) + "C"
-    print "Internal\ntemperature: {}C humidity: {}% soil moisture: {}%".format(DHT_temp, DHT_hum, soil_moist)
-    print "LED: {} PUMP: {} HEAT: {}".format(in1_state, in2_state, in3_state)
-    print "Set temperature: {}C Set moisture: {}% set temperature range: {}C".format(wanted_temp, wanted_soil, temp_offset)
+while 1:
+    try:
+        getFromDB()
+        sendToDB()
+        print "ding"
+        sleep(2)
+    except:
+        print "Zjebalo sie"
+        sleep(2)
+        continue
